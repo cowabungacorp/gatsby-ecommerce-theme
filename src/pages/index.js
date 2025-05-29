57ade0df4f20fb1516596a37b37a487570fd83ab
cowabungacorp <50673932+cowabungacorp@users.noreply.github.com>
2025-05-29 12:46:05 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,84 +30,24 @@ const IndexPage = () => {
     <Layout disablePaddingBottom>
 
 
+     <div class="container">
+        <div class="star-icon">★</div>
+        <h1>Estamos Quase Lá!</h1>
+        <p>
+            Prepare-se para uma experiência incrível! A <span class="store-name">Pulsar Azul</span>
+            está chegando com uma seleção especial de <span class="product-niche">produtos autistas</span>
+            cuidadosamente pensados para trazer conforto, desenvolvimento e alegria.
+        </p>
+        <p>
+            Estamos trabalhando duro para que tudo esteja perfeito. Em breve, você terá acesso a um universo de
+            possibilidades. Fique de olho!
+        </p>
+        <p class="text-sm text-gray-500 mt-4">
+            Agradecemos a sua paciência e entusiasmo!
+        </p>
+    </div>
+
      
-      {/* Collection Container */}
-      <div className={styles.collectionContainer}>
-        <Container size={'large'}>
-          <Title name={'New Collection'} />
-          <ProductCollectionGrid />
-        </Container>
-      </div>
-
-      {/* New Arrivals */}
-      <div className={styles.newArrivalsContainer}>
-        <Container>
-          <Title name={'New Arrivals'} link={'/shop'} textLink={'view all'} />
-          <ProductCardGrid
-            spacing={true}
-            showSlider
-            height={480}
-            columns={3}
-            data={newArrivals}
-          />
-        </Container>
-      </div>
-
-      {/* Highlight  */}
-      <div className={styles.highlightContainer}>
-        <Container size={'large'} fullMobile>
-          <Highlight
-            image={'/highlight.png'}
-            altImage={'highlight image'}
-            miniImage={'/highlightmin.png'}
-            miniImageAlt={'mini highlight image'}
-            title={'Luxury Knitwear'}
-            description={`This soft lambswool jumper is knitted in Scotland, using yarn from one of the world's oldest spinners based in Fife`}
-            textLink={'shop now'}
-            link={'/shop'}
-          />
-        </Container>
-      </div>
-
-      {/* Promotion */}
-      <div className={styles.promotionContainer}>
-        <Hero image={toOptimizedImage('/banner2.png')} title={`-50% off \n All Essentials`} />
-        <div className={styles.linkContainers}>
-          <Link to={'/shop'}>WOMAN</Link>
-          <Link to={'/shop'}>MAN</Link>
-        </div>
-      </div>
-
-      {/* Quote */}
-      <Quote
-        bgColor={'var(--standard-light-grey)'}
-        title={'about Sydney'}
-        quote={
-          '“We believe in two things: the pursuit of quality in everything we do, and looking after one another. Everything else should take care of itself.”'
-        }
-      />
-
-      {/* Blog Grid */}
-      <div className={styles.blogsContainer}>
-        <Container size={'large'}>
-          <Title name={'Journal'} subtitle={'Notes on life and style'} />
-          <BlogPreviewGrid data={blogData} />
-        </Container>
-      </div>
-
-      {/* Promotion */}
-      <div className={styles.sustainableContainer}>
-        <Hero
-          image={toOptimizedImage('/banner3.png')}
-          title={'We are Sustainable'}
-          subtitle={
-            'From caring for our land to supporting our people, discover the steps we’re taking to do more for the world around us.'
-          }
-          ctaText={'read more'}
-          maxWidth={'660px'}
-          ctaStyle={styles.ctaCustomButton}
-        />
-      </div>
 
       {/* Social Media */}
       <div className={styles.socialContainer}>
